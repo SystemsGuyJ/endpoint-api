ad207934cbbb0eabaa1e11198e55a4315481f5ae
Gifted-Labs <juliusadjeteysowah@gmail.com>
2023-09-10 00:12:56 +0000

    This is the initial commit

---
+++ src/main/java/com/zuriinternship/thesystemsguy/EndpointAPI/EndpointApiApplication.java
@@ -0,0 +1,13 @@
+package com.zuriinternship.thesystemsguy.EndpointAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndpointApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zuriinternship/thesystemsguy/EndpointAPI/EndpointApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuriinternship.thesystemsguy.EndpointAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
